15d8471a1f420b8c7e6cb8347ea2e1402f6453f1
miguelangelfeo <mfeomerchan@gmail.com>
2026-05-25 13:30:40 -0500

    Arregla ruta

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/parcial-2-Ayala217/'
+  base: '/fullcycle-predict/'
 })
